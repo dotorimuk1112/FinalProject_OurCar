d4c13697af50bfd7dedf1d64a7f0fe51e22013f9
HanSangMin <sorkdhks33@gmail.com>
2024-04-11 17:55:27 +0900

    판매 게시글 CRD 기능 구현 완료 및 마이페이지 기능 구현

--- final_project/sales/views/base_views.py
+++ final_project/sales/views/base_views.py
@@ -15,7 +15,7 @@ def index(request):
     car_list = CarSalesPost.objects.order_by('-create_date')
     if kw:
         car_list = car_list.filter(
-            Q(subject__icontains=kw)   # 제목 검색
+            Q(MNAME__icontains=kw)   # 제목 검색
 
         ).distinct()
     
@@ -30,7 +30,6 @@ def detail(request, post_id):
     context = {'CarSalesPost': car_sales_post}
     return render(request, 'sales/sales_detail.html', context)
 
-
 @login_required(login_url='common:login')
 def my_page(request):
     user = request.user
@@ -41,29 +40,56 @@ def my_page(request):
     except TestSangmin.DoesNotExist:
         test_sangmin_instance = None
         
-    liked_questions = CarSalesPost.objects.filter(buyer=user)
+    # 사용자가 좋아하는 차량 가져오기
+    liked_car = CarSalesPost.objects.filter(buyer=user)
+    
+    # 사용자가 판매한 차량 가져오기
+    user_cars_for_sale = CarSalesPost.objects.filter(seller=user)
     
     # 페이지네이션 추가
-    page = request.GET.get('page', 1)  # 현재 페이지 번호
-    paginator = Paginator(liked_questions, 8)  # 페이지당 12개씩 보여주기
-    page_obj = paginator.get_page(page)
+    liked_car_page = request.GET.get('liked_car_page', 1)  # 좋아하는 차량 페이지 번호
+    user_cars_for_sale_page = request.GET.get('user_cars_for_sale_page', 1)  # 판매 중인 차량 페이지 번호
+    
+    liked_car_paginator = Paginator(liked_car, 8)  # 좋아하는 차량 페이지당 8개씩 보여주기
+    user_cars_for_sale_paginator = Paginator(user_cars_for_sale, 8)  # 판매 중인 차량 페이지당 8개씩 보여주기
+    
+    liked_car_page_obj = liked_car_paginator.get_page(liked_car_page)
+    user_cars_for_sale_page_obj = user_cars_for_sale_paginator.get_page(user_cars_for_sale_page)
+    
+    # 좋아하는 차량과 판매 중인 차량의 개수
+    liked_car_count = liked_car.count()
+    user_cars_for_sale_count = user_cars_for_sale.count()
     
     # 사용자의 프로필 이미지를 가져옵니다.
     profile_image = user.profile_image
+    
     if request.method == 'POST':
-        print("post----------------------")
         form = ProfileImageForm(request.POST, request.FILES, instance=request.user)
-        print(request.FILES)
         if form.is_valid():
             form.save()
-            return render(request, 'sales/my_page.html', {'liked_questions': page_obj, 'test_sangmin_instance': test_sangmin_instance, 'profile_image': profile_image, 'profile_image_form': form})  # Render the user's profile page
-            # return redirect("/")  # Render the user's profile page
+            return render(request, 'sales/my_page.html', {
+                'liked_car': liked_car_page_obj,
+                'user_cars_for_sale': user_cars_for_sale_page_obj,
+                'test_sangmin_instance': test_sangmin_instance,
+                'profile_image': profile_image,
+                'profile_image_form': form,
+                'liked_car_count': liked_car_count,
+                'user_cars_for_sale_count': user_cars_for_sale_count
+            })  
     else:
         form = ProfileImageForm(instance=request.user)
-        print("get----------------------")
 
-    return render(request, 'sales/my_page.html', {'liked_questions': page_obj, 'test_sangmin_instance': test_sangmin_instance, 'profile_image': profile_image, 'profile_image_form': form})
-    
+    return render(request, 'sales/my_page.html', {
+        'liked_car': liked_car_page_obj,
+        'user_cars_for_sale': user_cars_for_sale_page_obj,
+        'test_sangmin_instance': test_sangmin_instance,
+        'profile_image': profile_image,
+        'profile_image_form': form,
+        'liked_car_count': liked_car_count,
+        'user_cars_for_sale_count': user_cars_for_sale_count
+    })
+
+
 # @login_required(login_url='common:login')
 # def upload_profile_image(request):
 #     if request.method == 'POST':
