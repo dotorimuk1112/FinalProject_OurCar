32ff39d5b0707333926a7d2f3c9cdc2cd2f51fb5
dotorimuk1112 <chhyky12@gmail.com>
2024-04-23 16:06:10 +0900

    gitignore update

--- final_project/common/static/car_price_pred.py
+++ final_project/common/static/car_price_pred.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 def car_price_pred_model(car):
-    with open('car_price_prediction_models_v2.pkl', 'rb') as f:
+    with open('car_price_prediction_models_RF.pkl', 'rb') as f:
         loaded_model = pickle.load(f)
 
     try:
@@ -97,7 +97,7 @@ def car_price_pred_model(car):
         return error_message
     
 def car_price_pred_model_10000(car):
-    with open('car_price_prediction_models_v2.pkl', 'rb') as f:
+    with open('car_price_prediction_models_RF.pkl', 'rb') as f:
         loaded_model = pickle.load(f)
 
     try:
--- final_project/config/settings.py
+++ final_project/config/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'common' / 'static',
+    BASE_DIR / 'sales' / 'static',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
