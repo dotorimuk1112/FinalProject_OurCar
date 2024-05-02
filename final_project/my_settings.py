b797794da36a3c48514c5fd0e7d3399440f0518d
HanSangMin <sorkdhks33@gmail.com>
2024-05-02 18:48:05 +0900

    jenkins practice_done5

--- final_project/my_settings.py
+++ final_project/my_settings.py
@@ -4,8 +4,16 @@ import json
 
 load_dotenv()
 
-DATABASES=json.loads(os.environ.get("DATABASES"))
-print(type(DATABASES))
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+    }
+}
 
 AWS_ACCESS_ID=os.environ.get("AWS_ACCESS_ID")
 AWS_SECRET_KEY=os.environ.get("AWS_SECRET_KEY")
