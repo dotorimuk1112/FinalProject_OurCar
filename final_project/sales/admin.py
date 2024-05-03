e5e6375f50444af61bb411cbc8e0c6d9b41a7f60
HanSangMin <sorkdhks33@gmail.com>
2024-05-03 13:45:30 +0900

    superuser regist

--- final_project/sales/admin.py
+++ final_project/sales/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import CarSalesPost
+from common.models import CustomUser, CarAPI,loan_rate_list
 # Register your models here.
+admin.site.register(CarSalesPost)
+admin.site.register(CustomUser)
+admin.site.register(CarAPI)
+admin.site.register(loan_rate_list)
